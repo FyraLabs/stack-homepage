304696c01c033c9828734ce6302657a4ea230b0e
addison <me@addi.lol>
2026-04-25 01:03:43 -0700

    chore: fix ultracite stylelint setup

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,1 +1,36 @@
-export { default } from 'ultracite/stylelint';
+import config from 'ultracite/stylelint';
+
+export default {
+	...config,
+	extends: [...config.extends, 'stylelint-config-html'],
+	ignoreFiles: ['**/*', '!**/*.css', '!**/*.html', '!**/*.svelte', '!**/*.svx'],
+	rules: {
+		...config.rules,
+		'at-rule-no-unknown': [
+			true,
+			{
+				ignoreAtRules: [
+					'tailwind',
+					'apply',
+					'layer',
+					'variants',
+					'responsive',
+					'screen',
+					'source',
+					'reference',
+					'plugin',
+					'theme'
+				]
+			}
+		]
+	},
+	overrides: [
+		{
+			files: ['**/*.html', '**/*.svelte', '**/*.svx'],
+			customSyntax: 'postcss-html',
+			rules: {
+				'no-invalid-position-declaration': null
+			}
+		}
+	]
+};
